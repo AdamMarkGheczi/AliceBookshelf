ec158b34bcd7d13747fdfe30540f09070bb9856e
Adam Gheczi <adamark03gmail.com>
2023-06-13 17:17:12 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Alice_Bookshelf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TextReader read = new StreamReader(@"..\..\..\Input.txt");
+            string inputString = read.ReadLine();
+
+            Stack<char> bookShelf = new Stack<char>();
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (inputString[i] == '\\')
+                {
+                    Queue<char> temp = new Queue<char>();
+                    char x;
+
+                    while((x = bookShelf.Pop()) != '/')
+                        temp.Enqueue(x);
+
+                    while(temp.Count > 0)
+                        bookShelf.Push(temp.Dequeue());
+                }
+                else
+                    bookShelf.Push(inputString[i]);
+            }
+
+            string output = string.Concat(bookShelf.ToArray().Reverse());
+
+            Console.WriteLine(output);
+        }
+    }
+}
